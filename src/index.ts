a06a725ca76c9521fd4b6f1cdf19254aeabdbc78
Baraka Larbi <laridev@gmail.com>
2020-05-17 13:41:16 +0100

    adding all pdf config

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,25 @@ import handlebars from 'handlebars';
 import path from 'path';
 
 interface configInterface {
-  emulator: any;
+  emulator?: any;
+  displayHeaderFooter?: boolean | undefined;
+  headerTemplate?: string | undefined;
+  footerTemplate?: string | undefined;
+  printBackground?: boolean | undefined;
+  format?: any;
+  margin?: marginInterface;
+  scale?: number | undefined;
+  landscape?: boolean | undefined;
+  pageRanges?: string | undefined;
+  width?: string | number | undefined;
+  height?: string | number | undefined;
+  preferCSSPageSize?: boolean | undefined;
+}
+interface marginInterface {
+  top?: string | number | undefined;
+  bottom?: string | number | undefined;
+  left?: string | number | undefined;
+  right?: string | number | undefined;
 }
 
 const compile = async (template: string, data: []) => {
@@ -23,18 +41,31 @@ export const generatePdf = async (
   const template: string = `${templateFolderPath}/${templateFileName}.${templateExtension}`;
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-
+  let content;
   if (templateExtension === 'hbs' || templateExtension === '.hbs') {
-    const content = await compile(template, []);
-    await page.setContent(content);
-    await page.emulateMediaType(config.emulator || 'screen');
+    content = await compile(template, []);
   } else {
     return 'this module only support hbs compilation only for now !!';
   }
 
-  return 'pdf file';
+  await page.setContent(content);
+  await page.emulateMediaType(config.emulator || 'screen');
 
-  // const pdf = await page.pdf({
-  //       config.pdfOptions
-  // })
+  const pdf = await page.pdf({
+    displayHeaderFooter: config.displayHeaderFooter,
+    headerTemplate: config.headerTemplate,
+    footerTemplate: config.footerTemplate,
+    printBackground: config.printBackground,
+    format: config.format,
+    margin: config.margin,
+    scale: config.scale,
+    landscape: config.landscape,
+    pageRanges: config.pageRanges,
+    width: config.width,
+    height: config.height,
+    preferCSSPageSize: config.preferCSSPageSize,
+  });
+
+  await browser.close();
+  return pdf;
 };
