bb5e7f97a1022a6dbb3f12158b3bbdc187428469
Baraka Larbi <laridev@gmail.com>
2020-05-17 13:24:07 +0100

    adding compile function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,37 @@
-export const generatePdf = (template: string, config: string, data: []) => {
+import * as fs from 'fs-extra';
+import * as puppeteer from 'puppeteer';
+import handlebars from 'handlebars';
+import path from 'path';
+
+interface configInterface {
+  emulator: any;
+}
+
+const compile = async (template: string, data: []) => {
+  const filePath = path.join(process.cwd(), template);
+  const html = await fs.readFile(filePath, 'utf-8');
+  return handlebars.compile(html)(data);
+};
+
+export const generatePdf = async (
+  templateFolderPath: string,
+  templateFileName: string,
+  templateExtension: string,
+  config: configInterface,
+  data: []
+) => {
+  const template: string = `${templateFolderPath}/${templateFileName}.${templateExtension}`;
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  const content = await compile(template, []);
+
+  await page.setContent(content);
+
+  await page.emulateMediaType(config.emulator || 'screen');
+
+  // const pdf = await page.pdf({
+  //       config.pdfOptions
+  // })
+
   return 'pdf generator';
 };
